9ac0c71902fba0ce21b8b9d1aaa2bb7a2c6f0032
abenjamin1313 <abenjamin1313@gmail.com>
2018-06-04 20:23:03 -0400

    text update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
     <nav className="navbar navbar-default navbar-static-top">
         <div className="container text-center">
-            <h1>Adam StockX React App Challenge</h1>
+            <h1>Adam React App Challenge</h1>
         </div>
     </nav>
 );
